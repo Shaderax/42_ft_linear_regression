f6ad04d419bd3dd43b180bae0e15a99bb6841a50
Nathan ROUZEVAL <nrouzeva@e1r4p1.42.fr>
2018-07-05 12:43:05 +0200

    Finish

---
+++ estimate.py
@@ -0,0 +1,47 @@
+from __future__ import division
+import csv
+import sys
+import math
+import matplotlib.pyplot as plt
+
+def input_km():
+    mileage = 0
+    while True:
+       print('Please enter a mileage: ')
+       try:
+           choice = input()
+       except EOFError:
+           error('EOF on input. Exit..')
+           sys.exit(0)
+       except:
+           error('Unknown error on input. Exit...')
+           sys.exit(0)
+       if type(choice) == int:
+           mileage = int(choice)
+           break
+           error('{}: Not a valid value'.format(choice))
+    return mileage
+
+def main():
+    filename = "results.csv"
+    teta0 = []
+    teta1 = []
+    estimate = 0
+    mileage = 0
+
+    with open(filename, "rb") as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        try:
+            for row in reader:
+                teta0.append(row[0])
+                teta1.append(row[1])
+        except csv.Error as e:
+            sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+        
+    mileage = input_km()
+    estimate = float(teta0[0]) + (float(teta1[0]) * mileage / 10000)
+
+    print('Price: {}'.format(int(estimate)))
+
+if __name__ == "__main__":
+    main()
